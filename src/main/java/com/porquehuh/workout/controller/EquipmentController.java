06160ebf6cb05abef2a8e3e68f024ca9201cc74c
Jesus <jesusabarrerag@gmail.com>
2021-02-16 22:35:28 -0600

    Added update and delete feature

--- src/main/java/com/porquehuh/workout/controller/EquipmentController.java
+++ src/main/java/com/porquehuh/workout/controller/EquipmentController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,5 +48,31 @@ public class EquipmentController {
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(equipment);
 	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		
+		if (equipmentService.isEquipmentExists(id)) {
+			equipmentService.deleteById(id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		} else {
+			return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+		}
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Equipment> update(@PathVariable Long id, @RequestBody Equipment equipment) {
+			
+		if (!equipmentService.isEquipmentExists(id)) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(equipment);
+		}
+		
+		equipment.setId(id);
+		equipmentService.save(equipment);
+		
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(equipment);
+		
+	}
+	
 
 }
